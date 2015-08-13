7bc5b4d9d9a0902902f5da1482bdb8bcce841576
Jennifer Bell & Evan Clough <student@epicodus.com>
2015-08-13 10:02:37 -0700

    Write spec to test saving and retrieving multiple contacts.

--- spec/contacts_spec.rb
+++ spec/contacts_spec.rb
@@ -9,4 +9,11 @@ describe('Contacts') do
     expect(test_contact.job_title()).to(eq("Professional Napper"))
     expect(test_contact.company()).to(eq("Feline Inc"))
   end
+
+  it('saves and makes retrievable multiple contacts') do
+    test_contact_1 = Contact.new({:first_name => "Taco", :last_name => "Cat", :job_title => "Professional Napper", :company => "Feline Inc"})
+    test_contact_2 = Contact.new({:first_name => "Burrito", :last_name => "Dog", :job_title => "Professional Licker", :company => "Canine SA"})
+    test_contact_3 = Contact.new({:first_name => "Empanada", :last_name => "Fish", :job_title => "Professional Finger Follower", :company => "Pescado Gmbh"})
+    expect(test_contact_2.job_title()).to(eq("Professional Licker"))
+  end
 end
