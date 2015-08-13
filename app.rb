0bd2c4ce437d2a61ce3ce6ccd8614af49243f0fc
Jennifer Bell & Evan Clough <student@epicodus.com>
2015-08-13 15:41:46 -0700

    Write post '/new_contact' route for adding new contact.

--- app.rb
+++ app.rb
@@ -12,3 +12,39 @@ get('/') do
   # binding.pry
   erb(:index)
 end
+
+post('/new_contact') do
+
+  first_name = params.fetch("first_name")
+  last_name = params.fetch("last_name")
+  job_title = params.fetch("job_title")
+  company = params.fetch("company")
+
+  street = params.fetch("street")
+  city = params.fetch("city")
+  state = params.fetch("state")
+  zip_code = params.fetch("zip_code")
+  address_type = params.fetch("address_type")
+
+  phone_number = params.fetch("phone_number")
+  phone_type = params.fetch("phone_type")
+
+  email = params.fetch("email")
+  email_type = params.fetch("email_type")
+
+  new_contact = Contact.new({:first_name => first_name, :last_name => last_name, :job_title => job_title, :company => company})
+
+  address_to_add = Address.new({:street => street, :city => city, :state => state, :zip_code => zip_code, :type => address_type})
+
+  #
+  #
+  # Phone.new(phone_number, phone_type).save()
+  # Email.new(email, email_type).save()
+  #
+  new_contact.new_address(address_to_add)
+
+  new_contact.save()
+
+  @contacts = Contact.all
+  erb(:index)
+end
