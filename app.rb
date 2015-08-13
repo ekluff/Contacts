88c637f42870540cc7feb82f41cf8bbdce02dbb0
Jennifer Bell & Evan Clough <student@epicodus.com>
2015-08-13 15:45:42 -0700

    Clarify app.rb by adding comments.

--- app.rb
+++ app.rb
@@ -15,33 +15,36 @@ end
 
 post('/new_contact') do
 
+  # params for new contact
   first_name = params.fetch("first_name")
   last_name = params.fetch("last_name")
   job_title = params.fetch("job_title")
   company = params.fetch("company")
 
+  # params for new address
   street = params.fetch("street")
   city = params.fetch("city")
   state = params.fetch("state")
   zip_code = params.fetch("zip_code")
   address_type = params.fetch("address_type")
 
+  # params for new phone
   phone_number = params.fetch("phone_number")
   phone_type = params.fetch("phone_type")
 
+  # params for new email
   email = params.fetch("email")
   email_type = params.fetch("email_type")
 
+  # creates Contact instance
   new_contact = Contact.new({:first_name => first_name, :last_name => last_name, :job_title => job_title, :company => company})
 
+  # creates Address instance to add to new_contact
   address_to_add = Address.new({:street => street, :city => city, :state => state, :zip_code => zip_code, :type => address_type})
 
-  #
-  #
-  # Phone.new(phone_number, phone_type).save()
-  # Email.new(email, email_type).save()
-  #
   new_contact.new_address(address_to_add)
+  new_contact.new_phone(phone_to_add)
+  new_contact.new_email(email_to_add)
 
   new_contact.save()
 
